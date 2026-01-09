941fc0b6f6b4bdeff58243255b7603405d65f7f8
TEEDA HEMANTH KUMAR <hemanthkumar199925@gmail.com>
2026-01-09 13:09:21 +0530

    Update 1. fifo.sv

--- 2. FIFO_verification/1. fifo.sv
+++ 2. FIFO_verification/1. fifo.sv
@@ -1,58 +1,69 @@
+// Code your design here
+// Code your testbench here
+// or browse Examples
 
-module fifo(input clk, rst, wr, rd,
-            input [7:0] din, output reg [7:0] dout,
-            output empty, full);
-  
-  // Pointers for write and read operations
-  reg [3:0] wptr = 0, rptr = 0;
-  
-  // Counter for tracking the number of elements in the FIFO
-  reg [4:0] cnt = 0;
-  
-  // Memory array to store data
-  reg [7:0] mem [15:0];
+`timescale 1ns/1ps
 
-  always @(posedge clk)
-    begin
-      if (rst == 1'b1)
-        begin
-          // Reset the pointers and counter when the reset signal is asserted
-          wptr <= 0;
-          rptr <= 0;
-          cnt  <= 0;
-        end
-      else if (wr && !full)
-        begin
-          // Write data to the FIFO if it's not full
-          mem[wptr] <= din;
-          wptr      <= wptr + 1;
-          cnt       <= cnt + 1;
-        end
-      else if (rd && !empty)
-        begin
-          // Read data from the FIFO if it's not empty
-          dout <= mem[rptr];
-          rptr <= rptr + 1;
-          cnt  <= cnt - 1;
-        end
+module sync_fifo(
+    input logic clk,rst,
+    input logic [7:0] data_in,
+    input logic write_en, read_en,
+    output logic [7:0] data_out,
+    output logic full,empty
+);
+
+  logic [3:0] write_ptr, read_ptr;
+  logic [7:0] mem [15:0] ;
+  logic [4:0] counter;
+
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        data_out <= 8'd0;
+        counter <= 5'd0;
+        write_ptr <= 4'd0;
+        read_ptr <= 4'd0;
+
     end
+  
+  else begin 
+    if ((write_en && !full) && (read_en && !empty)) 
+      begin
+        counter <= counter;
+        read_ptr <= read_ptr + 1;
+        write_ptr <= write_ptr + 1;
+        data_out <= mem[read_ptr];
+        mem[write_ptr] <= data_in;
+        
+  end
+    else if(read_en && !empty)
+     begin
+        data_out <= mem[read_ptr];
+        counter <= counter -1;
+        read_ptr <= read_ptr + 1;
+    end
+    else if (write_en && !full) begin     
+        mem[write_ptr] <= data_in;
+        counter <= counter + 1;
+        write_ptr <= write_ptr + 1;
+    end
+  end
 
-  // Determine if the FIFO is empty or full
-  assign empty = (cnt == 0) ? 1'b1 : 1'b0;
-  assign full  = (cnt == 16) ? 1'b1 : 1'b0;
+end
+  
+  assign full = (counter == 5'd16 ) ? 1'b1 : 1'b0;
+  assign empty = (counter == 5'd0 ) ? 1'b1 : 1'b0;
+
 
 endmodule
 
-//////////////////////////////////////
 
 // Define an interface for the FIFO
 interface fifo_if;
   
-  logic clock, rd, wr;         // Clock, read, and write signals
+  logic clk, rst, write_en, read_en;         // Clock, read, and write signals
   logic full, empty;           // Flags indicating FIFO status
   logic [7:0] data_in;         // Data input
   logic [7:0] data_out;        // Data output
-  logic rst;                   // Reset signal
 
   modport drv_mp (
         input clock,
